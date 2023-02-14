d9057b28ea8c96426ce4233604fa632613938e37
datdtnhcse <dat.dtnhcse@gmail.com>
2023-02-15 00:02:34 +0700

    concept done

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gunlaxy")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BORDER = pygame.Rect(WIDTH / 2 - 5, 0, 10, HEIGHT)
 FPS = 60
 VEL = 5
 SPACESHIP_WIDTH = 55
@@ -18,9 +20,30 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP, (S
 
 def draw_window(red, yellow):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
+    
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: # move left
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x: # move right
+        yellow.x += VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15: # move down
+        yellow.y += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: # move up
+        yellow.y -= VEL
+        
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width + 10: # move left
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH + 10:  # move right
+        red.x += VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15: # move down
+        red.y += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: # move up
+        red.y -= VEL
 
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -37,14 +60,10 @@ def main():
                 run = False
                 
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a]: # move left
-            yellow.x -= VEL
-        if keys_pressed[pygame.K_d]: # move right
-            yellow.x += VEL
-        if keys_pressed[pygame.K_s]: # move down
-            yellow.y += VEL
-        if keys_pressed[pygame.K_w]: # move up
-            yellow.y -= VEL
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+
+        
                 
 
         
