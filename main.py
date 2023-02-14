77dcaece4382640dc29c2956b396a8930db6c13f
datdtnhcse <dat.dtnhcse@gmail.com>
2023-02-14 20:58:01 +0700

    movement

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gunlaxy")
 
 WHITE = (255, 255, 255)
+FPS = 60
+VEL = 5
+SPACESHIP_WIDTH = 55
+SPACESHIP_HEIGHT = 40
+
+YELLOW_SPACESHIP = pygame.image.load(os.path.join("assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP = pygame.image.load(os.path.join("assets", "spaceship_red.png"))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+def draw_window(red, yellow):
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.display.update()
 
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     
+    clock = pygame.time.Clock()
     run = True
     
     # Game Loop
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 
-        WIN.fill(WHITE)
-        pygame.display.update()
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]: # move left
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d]: # move right
+            yellow.x += VEL
+        if keys_pressed[pygame.K_s]: # move down
+            yellow.y += VEL
+        if keys_pressed[pygame.K_w]: # move up
+            yellow.y -= VEL
+                
+
+        
+        
+        draw_window(red, yellow)
+        
 
     pygame.quit()
     
